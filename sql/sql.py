7b5d485be2f018a61f803acd0ee13fff883cbfda
yangpingyan <yangpingyan@gmail.com>
2018-12-30 17:37:54 +0800

    1

--- sql/sql.py
+++ sql/sql.py
@@ -6,6 +6,7 @@
 import os
 import json
 import pandas as pd
+import pymysql
 from sshtunnel import SSHTunnelForwarder
 from sqlalchemy import create_engine
 
@@ -32,7 +33,19 @@ def sql_connect(database_name, sql_file, ssh_pkey=None):
         sql_conn = create_engine(
             'mysql+pymysql://{}:{}@127.0.0.1:{}/{}'.format(sql_user, sql_password, server.local_bind_port, database_name))
         print("Access MySQL with SSH tunnel forward")
-
+        if True:
+            sql_conn = pymysql.Connect(
+                host="127.0.0.1",
+                port=server.local_bind_port,
+                user=sql_user,
+                password=sql_password,
+                db=database_name,
+                charset='utf8'
+            )
+        else:
+            sql_conn = create_engine(
+                'mysql+pymysql://{}:{}@127.0.0.1:{}/{}'.format(sql_user, sql_password, server.local_bind_port,
+                                                               database_name))
     return sql_conn
 
 if __name__ == '__main__':
